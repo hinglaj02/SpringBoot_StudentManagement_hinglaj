e7c013b67a24934641eff18aebbff8dba3c19cba
hinglaj02 <95147773+hinglaj02@users.noreply.github.com>
2022-04-09 23:55:01 +0530

    Update student_managementsystm.sql

--- student_managementsystm.sql
+++ student_managementsystm.sql
@@ -1,7 +1,7 @@
 
 create database stud;
 
--- Change encrypted value to the password that you want to use to login - https://bcrypt-generator.com/
+-- note - PASSWORD IS SAME AS USERNAME AND Change encrypted value to the password that you want to use to login - https://bcrypt-generator.com/
 
 use stud;
 select * from roles;
@@ -9,6 +9,7 @@ select * from users;
 insert into roles values (1,'ADMIN');
 insert into roles values (2,'USER');
 insert into users values(1,'$2a$12$Q//GY.SfSzqTRWPuIZ7sw.01LH1GCtqwXQqDdWGJ4SzRIfC3VTa0q','abc');
+
 insert into users values(2,'$2a$12$hQEp8335QzFBtDNxcPt38e38LgeSzLafkqYLWDT5AYxDps3o/lxuO','hinglaj');
 use stud;
 
